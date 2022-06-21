181bfd24457f81984ea4b5d7ef3fb1b6802c3813
Jason Fuhrer <jf.33@hotmail.com>
2022-06-21 08:55:22 -0400

    set fetch options no-cors

--- src/fetch.js
+++ src/fetch.js
@@ -1,10 +1,32 @@
-async function getMonkeys() {
+async function getMonkey() {
     let url = 'https://statsnite.com/api/btd/v3/tower/dart-monkey'
+    let ops = {
+        mode: 'no-cors'
+    }
+
     try {
-        let res = await fetch(url)
+        let res = await fetch(url, ops)
         return await res.json()
     } catch (error) {
         console.log(error)
     }
 }
 
+async function renderMonkey() {
+    let monkey = await getMonkey()
+    let html = ''
+
+    
+
+    let htmlSegment = 
+        `<div class="monkey">
+            <h2>${monkey.id}</h2>
+        </div>`
+
+    html += htmlSegment
+
+    let container = document.getElementById('container')
+    container.innerHTML = html
+}
+
+renderMonkey();
